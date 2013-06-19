339b30b124f23a2a3529880d83741eb838b5d234
Joseph Workman <joeworkmn@gmail.com>
2013-06-19 00:14:33 -1000

    DataTables fnDeleteRow working now

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -2,14 +2,23 @@ module TicketsHelper
 
    # TODO Create edit_ticket_link
 
-   def delete_ticket_link(ticket, label="delete")
-      link_to(label, ticket, method: :delete, data: { confirm: "Really delete?" })
+   def delete_ticket_link(ticket, opts={})
+      default_opts = { from: 'edit', method: :delete, data: {confirm: "Really delete?"} }
+
+      opts = from = default_opts.merge(opts)
+
+      opts = from.slice!(:from)
+
+      #link_to("delete", ticket, opts)
+      link_to("delete", ticket_path(ticket, from), opts)
    end
 
 
    def destroy_ticket_redirect(ticket)
-      path = (request.referer == ticket_url(ticket) || 
-              request.referer == edit_ticket_url(ticket)) ? tickets_path : :back
+      #path = (request.referer == ticket_url(ticket) || 
+      #        request.referer == edit_ticket_url(ticket)) ? tickets_path : :back
+
+      path = (params[:from] == 'edit' || params[:from] == 'show') ? tickets_path : :back
 
       redirect_to path, notice: "Ticket has been deleted."
    end
