66f3900eeef4c0c98564a03d0d4ed88e8e22f2e7
Joseph Workman <joeworkmn@gmail.com>
2013-07-28 20:13:33 -1000

    decrease font size for each day in calendar

--- app/helpers/schedules_helper.rb
+++ app/helpers/schedules_helper.rb
@@ -5,16 +5,33 @@ module SchedulesHelper
    end
 
 
+   # Returns only day shift entries.
    def day_shift_entries_for(day)
-      day_shift = day.select { |e| e.day_or_night == 'day' }
+      day.select { |e| e.day_shift? }
    end
 
+
+   # Returns only night shift entries.
    def night_shift_entries_for(day)
-      night_shift = day.select { |e| e.day_or_night == 'night' }
+      day.select { |e| e.night_shift? }
    end
 
 
+   # Displays the label for a particular day in the schedule form
    def schedule_day_label(date)
       Date::DAYNAMES[date.wday] + " " + date.to_formatted_s(:long)
+   end
+
+
+   # Used in #show to display schedule for a single day.
+   def schedule_for_day(entries, time_shift)
+      html = ""
+      entries.each do |e|
+         if e.day_or_night == time_shift
+            html += content_tag(:u, e.shift_name)
+            html += content_tag(:p, e.staff_name)
+         end
+      end
+      raw(html)
    end
 end
--- app/models/schedule_entry.rb
+++ app/models/schedule_entry.rb
@@ -18,8 +18,21 @@ class ScheduleEntry < ActiveRecord::Base
    belongs_to :shift
    belongs_to :staff
 
+   delegate :name, :fname, :lname, to: :staff, prefix: true, allow_nil: true
+   delegate :name, to: :shift, prefix: true, allow_nil: true
+
    validates_presence_of :date, :day_or_night, :interval_position, :schedule_id, :shift_id
 
    scope :night_shift, -> { where(day_or_night: 'night') }
    scope :day_shift, -> { where(day_or_night: 'day') }
+
+
+   def day_shift?
+      day_or_night == 'day'
+   end
+
+
+   def night_shift?
+      day_or_night == 'night'
+   end
 end
